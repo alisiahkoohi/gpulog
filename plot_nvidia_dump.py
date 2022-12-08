6ff6975fbed5f07fbb7d34b74f4aaa87551fac47
Ali Siahkoohi <ali.siahkoohi@gmail.com>
2022-12-08 17:13:25 -0600

    ignore occasional NaNs and high-res image

--- plot_nvidia_dump.py
+++ plot_nvidia_dump.py
@@ -8,14 +8,25 @@ import matplotlib.pyplot as plt
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('statfile', type=Path)
-    parser.add_argument('--filter-ids', help="If given, only display GPU's with these ids (numbered after bus id).", type=int, nargs='+')
+    parser.add_argument(
+        '--filter-ids',
+        help=
+        "If given, only display GPU's with these ids (numbered after bus id).",
+        type=int,
+        nargs='+')
     args = parser.parse_args()
 
-    stats = pd.read_csv(args.statfile, parse_dates=True, skipinitialspace=True, index_col=0)
+    stats = pd.read_csv(args.statfile,
+                        parse_dates=True,
+                        skipinitialspace=True,
+                        index_col=0)
     for col_name in ('utilization.gpu [%]', 'utilization.memory [%]'):
         stats[col_name] = stats[col_name].str.rstrip(' %').astype(float) / 100
-
-    bus_ids = {bus_id:i for i,bus_id in enumerate(sorted(stats['pci.bus_id'].unique()))}
+    stats = stats.dropna()
+    bus_ids = {
+        bus_id: i
+        for i, bus_id in enumerate(sorted(stats['pci.bus_id'].unique()))
+    }
 
     stats['gpu_id'] = [bus_ids[bus_id] for bus_id in stats['pci.bus_id']]
     if args.filter_ids:
@@ -24,23 +35,35 @@ def main():
             stats = stats.drop(stats[stats['gpu_id'] == i].index)
 
     if len(stats) < 1:
-        raise RuntimeError("No values to display, did you filter out all GPU ids?")
+        raise RuntimeError(
+            "No values to display, did you filter out all GPU ids?")
 
-    fig, (ax_compute, ax_mem) = plt.subplots(2, 1, sharex='col')
+    fig, (ax_compute, ax_mem) = plt.subplots(2,
+                                             1,
+                                             sharex='col',
+                                             figsize=(12, 6))
     plt.suptitle(f'Utilization statistics from {args.statfile.name}')
 
-    stats.groupby('gpu_id')['utilization.gpu [%]'].plot(ax=ax_compute)
-    ax_compute.set_ylim(0, 1.05)
+    stats.groupby('gpu_id')['utilization.gpu [%]'].plot(ax=ax_compute, lw=0.1)
+    ax_compute.set_ylim(-0.01, 1.01)
     ax_compute.set_ylabel('GPU utilization')
-    stats.groupby('gpu_id')['utilization.memory [%]'].plot(ax=ax_mem)
-    ax_mem.set_ylim(0, 1.05)
+    stats.groupby('gpu_id')['utilization.memory [%]'].plot(ax=ax_mem, lw=0.1)
+    ax_mem.set_ylim(-0.01, 1.01)
     ax_mem.set_ylabel('Memory utilization')
 
-    # Are we sure the order of the plots are the same so that the legend is correct for both subplots?
-    plt.legend(title="gpu_id")
+    # Are we sure the order of the plots are the same so that the legend is
+    # correct for both subplots?
+    leg = plt.legend(loc='lower right', fontsize=7)
+    for line in leg.get_lines():
+        line.set_linewidth(1.0)
+    ax_mem.set_ylim(-0.01, 1.01)
     plt.tight_layout()
-    plt.subplots_adjust(top=0.9)
-    plt.show()
+    fig.savefig('gpu_usage.png',
+                dpi=2000,
+                pad_inches=.05,
+                bbox_inches="tight",
+                format="png")
+
 
 if __name__ == '__main__':
     main()
