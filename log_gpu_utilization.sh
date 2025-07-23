0ce91e684a6dc1073c8a6f93350994f405713084
Ali Siahkoohi <ali.siahkoohi@gmail.com>
2025-07-23 12:24:44 -0500

    show actual memory instead of percentage

--- log_gpu_utilization.sh
+++ log_gpu_utilization.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-nvidia-smi --query-gpu=timestamp,pci.bus_id,utilization.gpu,utilization.memory --format=csv -lms 100 -f gpu_log_$(date "+%Y-%m-%d-%T").csv
+nvidia-smi --query-gpu=timestamp,pci.bus_id,utilization.gpu,utilization.memory,memory.used --format=csv -lms 100 -f gpu_log_$(date "+%Y-%m-%d-%T").csv
 
--- plot_nvidia_dump.py
+++ plot_nvidia_dump.py
@@ -20,8 +20,8 @@ def main():
                         parse_dates=True,
                         skipinitialspace=True,
                         index_col=0)
-    for col_name in ('utilization.gpu [%]', 'utilization.memory [%]'):
-        stats[col_name] = stats[col_name].str.rstrip(' %').astype(float) / 100
+    stats['utilization.gpu [%]'] = stats['utilization.gpu [%]'].str.rstrip(' %').astype(float) / 100
+    stats['memory.used [MiB]'] = stats['memory.used [MiB]'].str.rstrip(' MiB').astype(float)
     stats = stats.dropna()
     bus_ids = {
         bus_id: i
@@ -47,8 +47,7 @@ def main():
     stats.groupby('gpu_id')['utilization.gpu [%]'].plot(ax=ax_compute, lw=1)
     ax_compute.set_ylim(-0.01, 1.01)
     ax_compute.set_ylabel('GPU utilization')
-    stats.groupby('gpu_id')['utilization.memory [%]'].plot(ax=ax_mem, lw=1)
-    ax_mem.set_ylim(-0.01, 1.01)
+    stats.groupby('gpu_id')['memory.used [MiB]'].plot(ax=ax_mem, lw=1)
     ax_mem.set_ylabel('Memory utilization')
 
     # Are we sure the order of the plots are the same so that the legend is
@@ -56,7 +55,6 @@ def main():
     leg = plt.legend(loc='lower right', fontsize=7)
     for line in leg.get_lines():
         line.set_linewidth(1.0)
-    ax_mem.set_ylim(-0.01, 1.01)
     plt.tight_layout()
     fig.savefig('gpu_usage.png',
                 dpi=300,
